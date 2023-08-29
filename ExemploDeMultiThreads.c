7cf55f074d4f48676ed25ec0321a913910b95254
G-Castellari <138142348+G-Castellari@users.noreply.github.com>
2023-08-29 14:24:06 -0300

    Update ExemploDeMultiThreads.c

--- ExemploDeMultiThreads.c
+++ ExemploDeMultiThreads.c
@@ -9,88 +9,12 @@
 
 sem_t S;
 
-void *tabuada (void *num)
-{
-    sem_wait (&S);
-    int valor = *(int *)(num);
-    int i ;
-    printf ("\nThread da função de tabuada: %lu\n", pthread_self() );
-    for (i = 0; i <= 10; i++)
-    {
-        printf("Tabuada do %d * %d = %d\n", valor, i, valor * i);
-        sleep(1);
-    }
-    sem_post (&S);
-}
-
-void *fatorial (void *num)
-{
-    sem_wait (&S);
-    int number = *(int *)num;
-    int fatorial;
-    int aux = *(int *)num;
-    sleep(1);
-    printf ("\nThread da função fatorial: %lu\n", pthread_self() );
-    for(fatorial = 1; number > 1; number--)
-    {
-        fatorial *= number;
-    }
-    printf("\nO faltorial de %d é %d\n", aux, fatorial);
-    sem_post (&S);
-}
-
-void *parOuImpar (void *num)
-{
-    sem_wait (&S);
-   int number = *(int *)num;
-   sleep(1);
-   printf ("\nThread da função par ou ímpar: %lu\n", pthread_self() );
-   
-   if(number % 2 == 0){
-       printf("\nO numero %d é Par\n", number);
-   }
-   else
-   {
-       printf("\nO numero %d é Ímpar\n",number);
-   }
-   sem_post (&S);
-}
-
-void *funcaoDeDobro (void *num)
-{
-    sem_wait (&S);
-    int number = *(int *)num;
-    sleep(1);
-    printf ("\nThread da função de dobro: %lu\n", pthread_self() );
-    printf("\nO dobro de %d = %d\n", number, number*2);
-    sem_post (&S);
-}
-
-void *elevarNumero (void *num)
-{
-    sem_wait (&S);
-    int numeber = *(int *)num;
-    sleep(1);
-    printf ("\nThread da função de elevar número ao quadrado: %lu\n", pthread_self() );
-    int n1;
-    
-    n1 = pow(numeber, 2);
-    printf("\n%d elevado ao quadrado é: %d\n", numeber, n1);
-    sem_post (&S);
-}
-
-void *jogoDeDados()
-{
-    sem_wait (&S);
-    int num;
-    sleep(1);
-    printf ("\nThread da função de jogo do dado: %lu\n", pthread_self() );
-    srand(time(NULL));
-    num = rand()%6 + 1;
-    printf("\nFace do dado: %d\n", num);
-    sem_post (&S);
-}
-
+void *tabuada (void *num);
+void *fatorial (void *num);
+void *parOuImpar (void *num);
+void *funcaoDeDobro (void *num);
+void *elevarNumero (void *num);
+void *jogoDeDados();
 
 int main ()
 {
@@ -118,4 +42,86 @@ int main ()
     pthread_join(jogoDeDadosThread, NULL);
     
     return 0;
+}
+
+void *tabuada (void *num)
+{
+    sem_wait (&S);
+    int valor = *(int *)(num);
+    int i ;
+    printf ("\nThread da função de tabuada: %lu\n", pthread_self() );
+    for (i = 0; i <= 10; i++)
+    {
+        printf("Tabuada do %d * %d = %d\n", valor, i, valor * i);
+        sleep(1);
+    }
+    sem_post (&S);
+}
+
+void *fatorial (void *num)
+{
+    sem_wait (&S);
+    int number = *(int *)num;
+    int fatorial;
+    int aux = *(int *)num;
+    sleep(1);
+    printf ("\nThread da função fatorial: %lu\n", pthread_self() );
+    for(fatorial = 1; number > 1; number--)
+    {
+        fatorial *= number;
+    }
+    printf("O faltorial de %d é %d\n", aux, fatorial);
+    sem_post (&S);
+}
+
+void *parOuImpar (void *num)
+{
+    sem_wait (&S);
+   int number = *(int *)num;
+   sleep(1);
+   printf ("\nThread da função par ou ímpar: %lu\n", pthread_self() );
+   
+   if(number % 2 == 0){
+       printf("O numero %d é Par\n", number);
+   }
+   else
+   {
+       printf("O numero %d é Ímpar\n",number);
+   }
+   sem_post (&S);
+}
+
+void *funcaoDeDobro (void *num)
+{
+    sem_wait (&S);
+    int number = *(int *)num;
+    sleep(1);
+    printf ("\nThread da função de dobro: %lu\n", pthread_self() );
+    printf("O dobro de %d = %d\n", number, number*2);
+    sem_post (&S);
+}
+
+void *elevarNumero (void *num)
+{
+    sem_wait (&S);
+    int numeber = *(int *)num;
+    sleep(1);
+    printf ("\nThread da função de elevar número ao quadrado: %lu\n", pthread_self() );
+    int n1;
+    
+    n1 = pow(numeber, 2);
+    printf("%d elevado ao quadrado é: %d\n", numeber, n1);
+    sem_post (&S);
+}
+
+void *jogoDeDados()
+{
+    sem_wait (&S);
+    int num;
+    sleep(1);
+    printf ("\nThread da função de jogo do dado: %lu\n", pthread_self() );
+    srand(time(NULL));
+    num = rand()%6 + 1;
+    printf("Face do dado: %d\n", num);
+    sem_post (&S);
 }
